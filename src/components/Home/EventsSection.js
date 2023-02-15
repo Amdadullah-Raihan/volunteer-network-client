a0a1414d3604e313cc8a74a089b54a1c33a115e1
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-02-16 00:32:09 +0600

    aah finalygit add . individual color genarated for every individual even div

--- src/components/Home/EventsSection.js
+++ src/components/Home/EventsSection.js
@@ -3,27 +3,44 @@ import useEvents from '../../hooks/useEvents';
 
 const EventsSection = () => {
     const [events, isLoading] = useEvents()
-    
+
+
+    //genarating individual color for each div
+
+    let randomColor;
+    const genarateColor = () =>{
+        const colors = ['#FFBD3E', '#FF7044', '#3F90FC', '#421FCF']
+        let randomNum = Math.floor(Math.random() * colors.length);
+        randomColor = colors[randomNum];
+        return randomColor;
+    }
+
+
 
     return (
-        <div className='relative w-[100vw] h-auto p-8 lg:container mx-auto'>
-           <div className="absolute top-[-50%] z-10  grid grid-cols-4 w-full  gap-4 ">
-            
-             {   
-             !events?
-             <div>
-                Loading Events...
-             </div>
-             : events?.map(event=>
-                    
-                    <div key={event._id} className='shadow-lg h-[250px]'>
-                        <img src={event.image} alt={event.image.name} className='h-[80%] bg-red-50' />
-                        <h5 className='w-full bg-red-100 h-[25%] items-center flex justify-center'>{event.title}</h5>
+        <div className='relative w-[100vw] lg:container mx-auto mb-6'>
+            <div className="absolute top-[-120px] z-10  grid grid-cols-4 w-full gap-4">
 
-                    </div>
-                    )
-             }
-           </div>
+                {
+                    !events ?
+                        <div>
+                            <p className='text-xl '>Laoding Events...</p>
+                        </div>
+                        : events?.map(event =>
+
+                            <div key={event._id} className='shadow-lg h-[250px] rounded-[10px]'>
+                                
+                                 <div className="hidden">
+                                       { genarateColor()}
+                                 </div>
+                               
+                                <img src={event.image} alt={event.image.name} className='h-[75%] bg-white rounded-[10px]' />
+                                <h5 className={`w-full h-[25%] items-center flex text-white uppercase font-semibold justify-center rounded-b-[10px]`} style={{backgroundColor:`${randomColor}`}} >{event.title}</h5>
+
+                            </div>
+                        )
+                }
+            </div>
         </div>
     );
 };
