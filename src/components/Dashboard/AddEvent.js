a4acb7f8b73b3c2b12b0f26b02a1c1d77c011e50
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-02-15 20:17:40 +0600

    calander adden inside add-event without functionality

--- src/components/Dashboard/AddEvent.js
+++ src/components/Dashboard/AddEvent.js
@@ -8,19 +8,22 @@ const AddEvent = () => {
 
     console.log(value);
     return (
-        <section className='m-20 p-10 bg-white'>
-            <div className='grid grid-cols-2 gap-6 '>
+        <section className='m-20 p-10 bg-white max-w-[800px] rounded-[20px] '>
+            <div className='grid grid-cols-1 gap-6'>
                 <div className='text-start'>
                     <p>Event Title</p>
                     <input className='border w-full px-4 py-2' type="text" name="" id="" placeholder='Enter Title' />
                 </div>
                 <div className='text-start'>
                     <p>Event Date</p>
-                    <input className='border w-full px-4 py-2' type="text" name="" id="" placeholder={new Date()}/>
+                    <input className='border w-full px-4 py-2' type="text" name="" id="" onClick={()=>{setOpen(!open)}} placeholder={new Date()}/>
+                    {
+                        open && <Calendar className='max-w-[500px] shadow-lg p-8 ' onChange={onChange} value={value}/>
+                    }
                 </div>
                 <div className='text-start'>
                     <p>Description</p>
-                    <input className='border w-full px-4  h-24' type="text" name="" id="" placeholder='Enter Description' />
+                    <textarea className='border w-full px-4 py-4 min-h-[100px] h-auto overflow-wrap' type="text" name="" id="" placeholder='Enter Description' />
                 </div>
                 <div className='text-start'>
                     <p>Banner</p>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,17 +5,28 @@ import RegisteredVolunteer from './RegisteredVolunteer';
 
 const Dashboard = () => {
     return (
-        <section className='flex '>
+        <section className='flex'>
             <div className='w-[30%]  px-10 py-4 h-[100vh]'>
                 <div className="text-start mt-10">
-                    <Link to='/dashboard/registered-volunteer'> <h5>Registered Volunteer List</h5></Link>
+                    <Link to='/dashboard/registered-volunteer' className='flex  text-[#207FEE]'>
+                        <span class="material-symbols-outlined text-md mr-2">
+                            supervisor_account
+                        </span>
+                        <h5> Registered Volunteer List</h5>
+                    </Link>
                     <br />
-                    <Link to='/dashboard/add-event'><h5>Add Event</h5></Link>
-                   
+                    <Link to='/dashboard/add-event' className='flex'>
+                        <span class="material-symbols-outlined text-md mr-2">
+                            add
+                        </span>
+                        <h5>Add Event</h5>
+                    </Link>
+
                 </div>
             </div>
             <div className=' bg-[#F5F6FA] w-full mt-4'>
                 <Routes>
+                    <Route path='/' element={<RegisteredVolunteer />} />
                     <Route path='/add-event' element={<AddEvent></AddEvent>} />
                     <Route path='/registered-volunteer' element={<RegisteredVolunteer />} />
                 </Routes>
